939be9c1447e29ad9a256d592e943a5c32ba1792
leo-gs <leo.g.stewart@gmail.com>
2017-10-28 00:02:07 -0700

    adding postgres db scripts

---
+++ postgres_db.py
@@ -0,0 +1,250 @@
+import datetime
+import json
+import os
+#import psycopg2
+from sql_utils import Field, Table
+import sys
+
+TABLE_PREFIX = "Harvey"
+DROP_EXISTING_TABLES = True
+INCLUDE_PARENT_TWEETS = True
+
+# Create table objects
+tweet_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True),
+	Field("truncated", "BOOLEAN"),
+	Field("isQuoteStatus", "BOOLEAN"),
+	Field("inReplyToStatusId", "BIGINT(20)"),
+	Field("favoriteCount", "INT(11)"),
+	Field("source", "TEXT"),
+	Field("coordinates_x", "FLOAT(8,8)"),
+	Field("coordinates_y", "FLOAT(8,8)"),
+	Field("inReplyToScreenName", "VARCHAR(45)"),
+	Field("retweetCount", "INT(11)"),
+	Field("inReplyToUserId", "BIGINT(20)"),
+	Field("lang", "VARCHAR(10)"),
+	Field("createdAt", "TIMESTAMP"),
+	Field("collectedAt", "TIMESTAMP")
+]
+tweet_table = Table("Tweet", tweet_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+tweet_foreign_key = tweet_table.get_field("tweetId")
+
+print(tweet_table.get_insert_statement())
+
+tweetuser_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=tweet_foreign_key, foreign_key_table=tweet_table),
+	Field("userId", "BIGINT(20)"),
+	Field("timeZone", "VARCHAR(20)"),
+	Field("verified", "BOOLEAN"),
+	Field("geoEnabled", "BOOLEAN"),
+	Field("followersCount", "INT(11)"),
+	Field("protected", "BOOLEAN"),
+	Field("lang", "VARCHAR(10)"),
+	Field("utcOffset", "INT(11)"),
+	Field("statusesCount", "INT(11)"),
+	Field("description", "TEXT"),
+	Field("friendsCount", "INT(11)"),
+	Field("name", "VARCHAR(128)"),
+	Field("favoritesCount", "INT(11)"),
+	Field("screenName", "VARCHAR(45)"),
+	Field("url", "TEXT"),
+	Field("createdAt", "TIMESTAMP"),
+	Field("location", "VARCHAR(256)"),
+	Field("collectedAt", "TIMESTAMP")
+]
+tweetuser_table = Table("TweetUser", tweetuser_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+
+tweethashtag_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=tweet_foreign_key, foreign_key_table=tweet_table),
+	Field("hashtag", "TEXT")
+]
+tweethashtag_table = Table("TweetHashtag", tweethashtag_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+
+tweetmention_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=tweet_foreign_key, foreign_key_table=tweet_table),
+	Field("userId", "BIGINT(20)"),
+	Field("mentionedId", "BIGINT(20)"),
+	Field("mentionedScreenName", "VARCHAR(45)"),
+	Field("mentionedName", "VARCHAR(128)")
+]
+tweetmention_table = Table("TweetMention", tweetmention_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+
+tweeturl_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=tweet_foreign_key, foreign_key_table=tweet_table),
+	Field("url", "TEXT"),
+	Field("display_url", "TEXT"),
+	Field("expanded_url", "TEXT")
+]
+tweeturl_table = Table("TweetUrl", tweeturl_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+
+tweetplace_table_fields = [
+	Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=tweet_foreign_key, foreign_key_table=tweet_table),
+	Field("boundingBoxJson", "JSON"),
+	Field("country", "VARCHAR(128)"),
+	Field("countryCode", "VARCHAR(10)"),
+	Field("fullName", "TEXT"),
+	Field("placeId", "BIGINT"),
+	Field("placeName", "VARCHAR(128)"),
+	Field("placeUrl", "TEXT")
+]
+tweetplace_table = Table("TweetPlace", tweetplace_table_fields, prefix=TABLE_PREFIX, if_not_exists=True)
+
+all_tables = [tweet_table, tweetuser_table, tweethashtag_table, tweetmention_table, tweeturl_table, tweetplace_table]
+
+def batch_insert(cursor, table, rows):
+	fields_required = len(table.fields)
+	for row in rows:
+		assert len(row)==fields_required, "Row has incorrect number of fields: " + str(row)
+
+	psycopg2.extras.execute_batch(cursor, table.get_insert_statement(), rows)
+
+#####################################
+#####################################
+
+def convert_timestring_to_timestamp(datetimestr):
+    datetime = parser.parse(datetimestr)
+    return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+def get_tweet_tuple(tweet, collectedAt):
+	tweetId = tweet["id_str"]
+	truncated = tweet["truncated"]
+	isQuoteStatus = tweet["is_quote_status"]
+	inReplyToStatusId = tweet["in_reply_to_status_id"]
+	favoriteCount = tweet["favorite_count"]
+	source = tweet["source"]
+	coordinates_x, coordinates_y = None, None
+	if "coordinates" in tweet and tweet["coordinates"] is not None:
+		coordinates_x = tweet["coordinates"]["coordinates"][0]
+		coordinates_y = tweet["coordinates"]["coordinates"][1]
+	inReplyToScreenName = tweet["in_reply_to_screen_name"]
+	retweetCount = tweet["retweet_count"]
+	inReplyToUserId = tweet["in_reply_to_user_id"]
+	tweet_lang = tweet["lang"]
+	tweet_createdAt = convert_timestring_to_timestamp(tweet["created_at"])
+
+	return (tweetId, truncated, isQuoteStatus, inReplyToStatusId, favoriteCount, source, coordinates_x, coordinates_y, inReplyToScreenName, retweetCount, inReplyToUserId, tweet_lang, tweet_createdAt, collectedAt)
+
+def get_tweetuser_tuple(tweet, collectedAt):
+	tweetId = tweet["id_str"]
+	tweetuser = tweet["user"]
+
+	userId = tweetuser["id_str"]
+	timeZone = tweetuser["time_zone"]
+	verified = tweetuser["verified"]
+	geoEnabled = tweetuser["geo_enabled"]
+	followersCount = tweetuser["followers_count"]
+	protected = tweetuser["protected"]
+	user_lang = tweetuser["lang"]
+	utcOffset = tweetuser["utc_offset"]
+	statusesCount = tweetuser["statuses_count"]
+	description = tweetuser["description"]
+	friendsCount = tweetuser["friends_count"]
+	name = tweetuser["name"]
+	favoritesCount = tweetuser["favourites_count"]
+	screenName = tweetuser["screen_name"]
+	url = tweetuser["url"]
+	user_createdAt = tweetuser["created_at"]
+	location = tweetuser["location"]
+
+	return (tweetId, userId, timeZone, verified, geoEnabled, followersCount, protected, user_lang, utcOffset, statusesCount, description, friendsCount, name, favoritesCount, screenName, url, user_createdAt, location, collectedAt)
+
+def get_tweetplace_tuple(tweet, collectedAt):
+	tweetplace = tweet.get("place", None)
+
+	if tweetplace:
+		boundingBoxJson = tweetplace["bounding_box"]
+		country = tweetplace["country"]
+		countryCode = tweetplace["country_code"]
+		fullName = tweetplace["full_name"]
+		placeId = tweetplace["id"]
+		placeName = tweetplace["name"]
+		placeUrl = tweetplace["url"]
+
+		return (tweetId, boundingBoxJson, country, countryCode, fullName, placeId, placeName, placeUrl)
+
+
+#####################################
+#####################################
+
+config = {}
+for line in open(sys.argv[1]).readlines():
+	key, value = line.strip().split("=")
+	config[key] = value
+
+db = psycopg2.connect(**config)
+cursor = db.cursor()
+
+for table in all_tables:
+	if DROP_EXISTING_TABLES:
+		cursor.execute(table.get_drop_statement)
+	cursor.execute(table.get_create_statement())
+
+input_json_dir = sys.argv[2]
+json_files = [f for f in os.listdir(input_json_dir) if (len(f) > 5 and f[-5:]==".json")]
+for f in json_files:
+	json_data = json.load(os.path.join(input_json_dir, f))
+
+	tweets = json_data["historic_tweets"]
+	collectedAt = convert_timestring_to_timestamp(tweet_json["utc_timestamp"])
+
+	tweet_tuples = []
+	tweetuser_tuples = []
+	tweetplace_tuples = []
+	tweethashtag_tuples = []
+	tweetmention_tuples = []
+	tweeturl_tuples = []
+
+	for tweet in tweets:
+		tweetId = tweet["id_str"]
+
+		tweet_tuples.append(get_tweet_tuple(tweet, collectedAt))
+		tweetuser_tuples.append(get_tweetuser_tuple(tweet, collectedAt))
+
+		if INCLUDE_PARENT_TWEETS:
+			if "retweeted_status" in tweet and tweet["retweeted_status"] is not None:
+				retweet = tweet["retweeted_status"]
+				# do some processing here
+
+			if "quoted_status" in tweet and tweet["quoted_status"] is not None:
+				quoted_tweet = tweet["quoted_status"]
+				# do some processing here
+
+		tweetplace = get_tweetplace_tuple(tweet, collectedAt)
+
+		entities = tweet.get("entities", None)
+		if entities:
+			if "hashtags" in entities:
+				for hashtag in entities["hashtags"]:
+					tweethashtag_tuples.append((tweetId, hashtag["text"]))
+
+			if "user_mentions" in entities:
+				for mention in entities["user_mentions"]:
+					mentionedId = mention["id_str"]
+					mentionedScreenName = mention["screen_name"]
+					mentionedName = mention["name"]
+
+					tweetmention_tuples.append((tweetId, userId, mentionedId, mentionedScreenName, mentionedName))
+
+			if "urls" in entities:
+				for url in entities["urls"]:
+					entity_url = url["url"]
+					display_url = url["display_url"]
+					expanded_url = url["expanded_url"]
+
+					tweeturl_tuples.append((tweetId, entity_url, display_url))
+
+	batch_insert(cursor, tweet_table, tweet_tuples)
+	batch_insert(cursor, tweetuser_table, tweeturl_table)
+	batch_insert(cursor, tweetplace_table, tweetplace_tuples)
+	batch_insert(cursor, tweethashtag_table, tweethashtag_tuples)
+	batch_insert(cursor, tweetmention_table, tweetmention_tuples)
+	batch_insert(cursor, tweeturl_table, tweeturl_tuples)
+
+	db.commit()
+
+
+
+
+
+
+
---
+++ sql_utils.py
@@ -0,0 +1,92 @@
+'''
+Examples:
+
+f1 = Field("tweetId", "BIGINT(20)", is_primary_key=True)
+f2 = Field("truncated", "BOOLEAN")
+f3 = Field("isQuoteStatus", "BOOLEAN", is_primary_key=True)
+f4 = Field("inReplyToStatusId", "BIGINT(20)")
+
+t = Table("test", [f1, f2, f3, f4])
+print(t.get_create_statement())
+
+f5 = Field("tweetId", "BIGINT(20)", is_primary_key=True, foreign_key=f1, foreign_key_table=t)
+t2 = Table("test2", [f5])
+print(t2.get_create_statement())
+
+'''
+
+
+class Field():
+
+	def __init__(self, name, datatype, is_primary_key=False, foreign_key=None, foreign_key_table=None):
+		self.name = name
+		self.datatype = datatype
+		self.is_primary_key = is_primary_key
+		self.foreign_key = foreign_key
+		self.foreign_key_table = foreign_key_table
+		
+		assert bool(foreign_key) == bool(foreign_key_table), "If foreign_key is assigned, foreign_key_table must also be assigned"
+
+	def get_insert_clause(self):
+		return "{name} {datatype}".format(
+				name = self.name,
+				datatype = self.datatype
+			)
+
+	def get_foreign_key_clause(self):
+		return "FOREIGN KEY ({field}) REFERENCES {other_table} ({other_field})".format(
+				field=self.name,
+				other_table=self.foreign_key_table.name,
+				other_field=self.foreign_key.name
+			)
+
+class Table():
+
+	def __init__(self, name, fields, prefix="", if_not_exists=False):
+		if prefix:
+			name = prefix + name
+		self.name = name
+		self.fields = fields
+		self.if_not_exists = if_not_exists
+
+	def get_drop_statement(self):
+		drop_statement = "DROP TABLE {table_name};".format(table_name=self.name)
+		return drop_statement
+
+	def get_create_statement(self):
+		if_not_exists_clause = " IF NOT EXISTS " if self.if_not_exists else ""
+		
+		field_strs = [field.get_insert_clause() for field in self.fields]
+
+		primary_keys = [field.name for field in self.fields if field.is_primary_key]
+		if primary_keys:
+			primary_key_clause = "PRIMARY KEY (" + ",".join(primary_key for primary_key in primary_keys) + ")"
+			field_strs.append(primary_key_clause)
+
+		foreign_keys = [field.get_foreign_key_clause() for field in self.fields if field.foreign_key]
+		for foreign_key in foreign_keys:
+			field_strs.append(foreign_key)
+
+		create_statement = "CREATE TABLE{if_not_exists}{table_name}({fields});".format(
+				if_not_exists=if_not_exists_clause,
+				table_name=self.name,
+				fields=", ".join(field_strs))
+
+		return create_statement
+
+	def get_insert_statement(self):
+		row_template = ",".join([field.name for field in self.fields])
+
+		insert_statement = "INSERT INTO {table_name} ({row_template}) VALUES ({value_flags})".format(
+			table_name=self.name,
+			row_template=row_template,
+			value_flags=",".join(["%s" for field in self.fields]))
+
+		return insert_statement
+
+	def get_field(self, name):
+		for field in self.fields:
+			if field.name == name:
+				return field
+
+
